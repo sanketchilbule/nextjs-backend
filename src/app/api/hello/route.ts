219baecc6f01cd3af7e0726efd94a87b66562f9c
sanket <schilbule1234@gmail.com>
2026-01-11 09:04:47 +0530

    added initial hello world code

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 
 
-export async function GET(req,res){
+export async function GET(){
     return NextResponse.json({result:"Hello world!" })
 }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
 
-export async function GET(req,res){
+export async function GET(){
     return NextResponse.json({name:"sanke", age:25, email:"sanke@gmail.com"})
 }
